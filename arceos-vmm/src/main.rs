3acd0161fe4b186db208eb6ff4251fac61f6b0b2
ChoHee <cc5281@126.com>
2024-08-30 10:39:59 +0800

    run arceos-parallel

--- arceos-vmm/src/main.rs
+++ arceos-vmm/src/main.rs
@@ -17,6 +17,7 @@ mod hal;
 // mod vmexit; temporarily removed
 
 use axerrno::{AxError, AxResult};
+use axhal::cpu;
 use axvm::config::{AxArchVCpuConfig, AxVCpuConfig, AxVMConfig};
 use axvm::{AxVM, AxVMPerCpu, GuestPhysAddr, HostPhysAddr, HostVirtAddr};
 use page_table_entry::MappingFlags;
@@ -27,8 +28,18 @@ use self::hal::AxVMHalImpl;
 #[percpu::def_percpu]
 pub static mut AXVM_PER_CPU: AxVMPerCpu<AxVMHalImpl> = AxVMPerCpu::new_uninit();
 
+use lazy_init::LazyInit;
+use alloc::sync::Arc;
+static mut HV_VM: LazyInit<Arc<AxVM<AxVMHalImpl>>> = LazyInit::new();
+
+use core::{sync::atomic::{AtomicUsize, Ordering}};
+
+static SYNC_VCPUS: AtomicUsize = AtomicUsize::new(0);
+
+static SMP: usize = axconfig::SMP;
+
 #[cfg_attr(feature = "axstd", no_mangle)]
-fn main() {
+fn main(cpu_id: usize) {
     println!("Starting virtualization...");
     info!("Hardware support: {:?}", axvm::has_hardware_support());
 
@@ -42,7 +53,8 @@ fn main() {
     debug!("{:#x?}", gpm);
 
     let config = AxVMConfig {
-        cpu_count: 1,
+        // cpu_count: 1,
+        cpu_count: 2,
         cpu_config: AxVCpuConfig {
             arch_config: AxArchVCpuConfig {
                 setup_config: (),
@@ -55,9 +67,54 @@ fn main() {
         // gpm : 0.into(),
     };
 
-    let vm = AxVM::<AxVMHalImpl>::new(config, 0, gpm.nest_page_table_root())
-        .expect("Failed to create VM");
+    // let vm = AxVM::<AxVMHalImpl>::new(config, 0, gpm.nest_page_table_root())
+    //     .expect("Failed to create VM");
+    unsafe {
+        HV_VM.init_by(AxVM::<AxVMHalImpl>::new(config, 0, gpm.nest_page_table_root()).expect("Failed to create VM"));
+    }
+    let vm = unsafe { HV_VM.get_mut_unchecked() };
+
+    info!("VCPU{} main hart ok and wait for sync", cpu_id);
+
+    SYNC_VCPUS.fetch_add(1, Ordering::Relaxed);
+    while !(SYNC_VCPUS.load(Ordering::Acquire) == SMP) {
+        core::hint::spin_loop();
+    }
+
+    info!("VCPU{} main hart sync done!!!", cpu_id);
+
     info!("Boot VM...");
     vm.boot().unwrap();
     panic!("VM boot failed")
+}
+
+#[no_mangle]
+fn secondary_main(cpu_id: usize){
+    assert!(cpu_id == 1);
+
+    debug!("HART{} entering hv_secondary_main", cpu_id);
+
+    while let None = unsafe { HV_VM.try_get() } {
+        core::hint::spin_loop();
+    }
+
+    let percpu = unsafe { AXVM_PER_CPU.current_ref_mut_raw() };
+    percpu.init(cpu_id).expect("Failed to initialize percpu state");
+    percpu
+        .hardware_enable()
+        .expect("Failed to enable virtualization");
+
+    info!("VCPU{} init ok and wait for sync", cpu_id);
+    SYNC_VCPUS.fetch_add(1, Ordering::Relaxed);
+
+    let vm = unsafe { HV_VM.get_mut_unchecked() };
+
+    vm.run_vcpu(cpu_id);
+
+    panic!("should not reach here");
+    
+    use axhal::arch::wait_for_irqs;
+    loop{
+        wait_for_irqs();
+    }
 }
